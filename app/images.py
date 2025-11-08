380214e49e34bad7f765fdd7c2ca6d843387881d
Devin Chaney <shout64@gmail.com>
2025-11-08 11:31:06 -0500

    Set up images file

--- app/images.py
+++ app/images.py
@@ -4,3 +4,8 @@ import os
 
 load_dotenv()
 
+imagekit = ImageKit(
+    private_key  = os.getenv("IMAGEKIT_PRIVATE_KEY"),
+    public_key   = os.getenv("IMAGEKIT_PUBLIC_KEY"),
+    url_endpoint = os.getenv("IMAGEKIT_URL_ENDPOINT")
+)
